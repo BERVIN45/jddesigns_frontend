07750256cef70659b0a74ebcf0c3caf9284bf391
BERVIN45 <john.802340@sxcce.edu.in>
2026-05-19 21:23:35 +0530

    new update width

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/shared/navbar/navbar';
 import { FooterComponent } from './components/shared/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {}
+export class App {
+  showScroll = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.showScroll = window.scrollY > 300;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+}
